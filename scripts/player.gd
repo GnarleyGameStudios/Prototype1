aa8fad83087666cd526c6b1f0827cbf2ae8c710c
GnarleyGameStudios <145240896+GnarleyGameStudios@users.noreply.github.com>
2023-09-17 17:44:57 -0400

    updated code to comply with godot naming conventions added player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,31 @@
 extends CharacterBody2D
 
 @export 
-var speed = 35
+var speed: int = 35
 
-func handleInput():
-	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = moveDirection*speed
+@onready
+var animations = $AnimationPlayer
+
+func handle_input():
+	var move_direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = move_direction*speed
 	
+func update_animation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction: String = "down"
+		
+		if velocity.x < 0 and abs(velocity.x) > abs(velocity.y):
+			direction = "left"
+		if velocity.x > 0 and abs(velocity.x) > abs(velocity.y):
+			direction = "right"
+		if velocity.y < 0:
+			direction = "up"
+			
+		animations.play("walk_" + direction)
+
 func _physics_process(delta):
-	handleInput()
+	handle_input()
 	move_and_slide()
+	update_animation()
